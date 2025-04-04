935c2e722c0ef4ddf089d34e193721422f8f0880
dion-t0 <dion.todorovski@gmail.com>
2025-04-04 17:58:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pgzrun
 import time
+import random 
 
 WIDTH=800
 HEIGHT=600
@@ -57,23 +58,61 @@ def make_items(number_of_extra_items):
 
 
 def get_option_to_create(number_of_extra_items):
-    pass
+    items_to_create = ["paper"]
+    for i in range(0, number_of_extra_items):
+        random_option = random.choice(ITEMS)
+        items_to_create.append(random_option)
+    return items_to_create
 
 def create_items(item_to_create):
-    pass
+    newitems=[]
+    for i in item_to_create:
+        items=Actor(i+"img")
+        newitems.append(items)
+    return newitems
 
-def layout_items(new_items):
-    pass
+def layout_items(items_to_layout):
+    gaps=len(items_to_layout)+1
+    gap_size=WIDTH/gaps
+    random.shuffle(items_to_layout)
+    for index, item in enumerate(items_to_layout):
+        new_x_pos = (index + 1) * gap_size
+        item.x = new_x_pos
 
-def animate_items(new_items):
-    pass
+def animate_items(items_to_animate):
+    global animations
+    for i in items_to_animate:
+        duration=START_SPEED-current_level
+        animation=animate(i,duration=duration, on_finished=handle_game_over, y=HEIGHT)
+        animations.append(animation)
 
+def handle_game_over():
+    global game_over
+    game_over=True
     
+def on_mouse_down(pos):
+    global items
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
 
+def handle_game_complete():
+    global current_level,items,animations,game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete=True
+    else:
+        current_level=current_level+1
+        items=[]
+        animations=[]
 
-
-
-
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
 
 
 
